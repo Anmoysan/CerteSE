c256d3da1aafb958c51aaaa7e1b19e9f8c2dd518
anmoysan <antonio.moya.sanchez.1996@gmail.com>
2018-02-25 03:45:02 +0100

    Incluyendo JQueryMask

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -114,6 +114,7 @@
 <script src="{{ asset('js/iziModal.js') }}"></script>
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.13/jquery.mask.js" type="text/javascript"></script>
 @stack('scripts')
 </body>
 </html>
