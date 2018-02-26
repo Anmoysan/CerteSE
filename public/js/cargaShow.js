fd742d2f249d0986947c5aa3a77d8876a4b0709d
Antonio Moya <antonio.moya.sanchez.1996@gmail.com>
2018-02-26 19:19:11 +0100

    rreglando error

--- public/js/cargaShow.js
+++ public/js/cargaShow.js
@@ -1,3 +1,83 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 53);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 53:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(54);
+
+
+/***/ }),
+
+/***/ 54:
+/***/ (function(module, exports, __webpack_require__) {
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
 $(function () {
     votes();
     modal();
@@ -6,41 +86,38 @@ $(function () {
 
 function coment() {
     $("#content").on({
-        keyup: function () {
-                comentar($("#content").val(), $("#event_id").val());
-        },
+        keyup: function keyup() {
+            comentar($("#content").val(), $("#event_id").val());
+        }
     });
 }
 
 function modal() {
     $("#reserva").iziModal({
         width: 750,
-        zindex: 999,
+        zindex: 999
     });
-
 
     $("#abrirReserva").on('click', function () {
         event.preventDefault();
         $("#reserva").iziModal('open');
-        $("#mapLugar").hide(1000, function () {
-        });
+        $("#mapLugar").hide(1000, function () {});
     });
 
     $("#unidad").on({
-        keyup: function () {
+        keyup: function keyup() {
             $("#cost").val(($("#unidad").val() * $("#costEvent").val()).toFixed(2));
         },
 
-        change: function (e) {
-            validateTarget(e.target)
+        change: function change(e) {
+            validateTarget(e.target);
         }
     });
 
-
     $("#createReserve").click(function (e) {
         e.preventDefault();
-        let enviarFormulario = true;
-        let formData = new FormData;
+        var enviarFormulario = true;
+        var formData = new FormData();
         formData.append('place', $("#place").val());
         formData.append('fecha', $("#fecha").val());
         formData.append('cost', $("#cost").val());
@@ -72,83 +149,83 @@ function modal() {
 
 function votes() {
     $("#estrella1").on({
-        mouseenter: function () {
+        mouseenter: function mouseenter() {
             $(this).css("background-color", "#cebb0a");
         },
 
-        mouseleave: function () {
+        mouseleave: function mouseleave() {
             $(this).css("background-color", "#dddddd");
         },
 
-        click: function () {
+        click: function click() {
             event.preventDefault();
             $("#vote").val(1);
             votar(1, $("#event_id").val());
-        },
+        }
     });
 
     $("#estrella2").on({
-        mouseenter: function () {
+        mouseenter: function mouseenter() {
             $("#estrella1").css("background-color", "#cebb0a");
             $(this).css("background-color", "#cebb0a");
         },
 
-        mouseleave: function () {
+        mouseleave: function mouseleave() {
             $("#estrella1").css("background-color", "#dddddd");
             $(this).css("background-color", "#dddddd");
         },
 
-        click: function () {
+        click: function click() {
             event.preventDefault();
             $("#vote").val(2);
             votar(2, $("#event_id").val());
-        },
+        }
     });
 
     $("#estrella3").on({
-        mouseenter: function () {
+        mouseenter: function mouseenter() {
             $("#estrella1").css("background-color", "#cebb0a");
             $("#estrella2").css("background-color", "#cebb0a");
             $(this).css("background-color", "#cebb0a");
         },
 
-        mouseleave: function () {
+        mouseleave: function mouseleave() {
             $("#estrella1").css("background-color", "#dddddd");
             $("#estrella2").css("background-color", "#dddddd");
             $(this).css("background-color", "#dddddd");
         },
 
-        click: function () {
+        click: function click() {
             event.preventDefault();
             $("#vote").val(3);
             votar(3, $("#event_id").val());
-        },
+        }
     });
 
     $("#estrella4").on({
-        mouseenter: function () {
+        mouseenter: function mouseenter() {
             $("#estrella1").css("background-color", "#cebb0a");
             $("#estrella2").css("background-color", "#cebb0a");
             $("#estrella3").css("background-color", "#cebb0a");
             $(this).css("background-color", "#cebb0a");
         },
 
-        mouseleave: function () {
+        mouseleave: function mouseleave() {
             $("#estrella1").css("background-color", "#dddddd");
             $("#estrella2").css("background-color", "#dddddd");
             $("#estrella3").css("background-color", "#dddddd");
             $(this).css("background-color", "#dddddd");
         },
 
-        click: function () {
+        click: function click() {
             event.preventDefault();
             $("#vote").val(4);
             votar(4, $("#event_id").val());
-        },
+        }
     });
 
     $("#estrella5").on({
-        mouseenter: function () {
+        mouseenter: function mouseenter() {
             $("#estrella1").css("background-color", "#cebb0a");
             $("#estrella2").css("background-color", "#cebb0a");
             $("#estrella3").css("background-color", "#cebb0a");
@@ -156,7 +233,7 @@ function votes() {
             $(this).css("background-color", "#cebb0a");
         },
 
-        mouseleave: function () {
+        mouseleave: function mouseleave() {
             $("#estrella1").css("background-color", "#dddddd");
             $("#estrella2").css("background-color", "#dddddd");
             $("#estrella3").css("background-color", "#dddddd");
@@ -164,7 +241,7 @@ function votes() {
             $(this).css("background-color", "#dddddd");
         },
 
-        click: function () {
+        click: function click() {
             event.preventDefault();
             $("#vote").val(5);
             votar(5, $("#event_id").val());
@@ -202,19 +279,38 @@ function comentar(content, evento) {
     });
 }
 
-
 function gestionarErrores(input, errores) {
-    let noEnviarFormulario = false;
+    var noEnviarFormulario = false;
     input = $(input);
-    if (typeof errores !== typeof undefined) {
+    if ((typeof errores === "undefined" ? "undefined" : _typeof(errores)) !== ( true ? "undefined" : _typeof(undefined))) {
         input.removeClass("is-invalid");
         input.addClass("is-invalid");
         input.nextAll(".invalid-feedback").remove();
-        for (let error of errores) {
-            input.after(`<div class="invalid-feedback">
-                <strong> ${error} </strong>
-            </div>`);
+        var _iteratorNormalCompletion = true;
+        var _didIteratorError = false;
+        var _iteratorError = undefined;
+
+        try {
+            for (var _iterator = errores[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+                var error = _step.value;
+
+                input.after("<div class=\"invalid-feedback\">\n                <strong> " + error + " </strong>\n            </div>");
+            }
+        } catch (err) {
+            _didIteratorError = true;
+            _iteratorError = err;
+        } finally {
+            try {
+                if (!_iteratorNormalCompletion && _iterator.return) {
+                    _iterator.return();
+                }
+            } finally {
+                if (_didIteratorError) {
+                    throw _iteratorError;
+                }
+            }
         }
+
         noEnviarFormulario = true;
     } else {
         input.removeClass("is-invalid");
@@ -225,7 +321,7 @@ function gestionarErrores(input, errores) {
 }
 
 function validateTarget(target) {
-    let formData = new FormData();
+    var formData = new FormData();
     formData.append(target.id, target.value);
     $(target).parent().addClass("spinner");
     axios.post('/reservar', formData).then(function (response) {
@@ -238,3 +334,7 @@ function validateTarget(target) {
         console.log(error);
     });
 }
+
+/***/ })
+
+/******/ });
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,14 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css');
+    .js('resources/assets/js/bootstrap.js', 'public/js')
+    .js('resources/assets/js/cargaShow.js', 'public/js')
+    .js('resources/assets/js/datetime.js', 'public/js')
+    .js('resources/assets/js/iziModal.js', 'public/js')
+    .js('resources/assets/js/loading.js', 'public/js')
+    .js('resources/assets/js/map.js', 'public/js')
+    .js('resources/assets/js/paginationEvents.js', 'public/js')
+    .js('resources/assets/js/paginationMyEvents.js', 'public/js')
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .sass('resources/assets/sass/home.css', 'public/css')
+    .sass('resources/assets/sass/iziModal.css', 'public/css');
