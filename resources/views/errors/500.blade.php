1e40647952be8c30d4de0217e922cb787b11aba6
anmoysan <antonio.moya.sanchez.1996@gmail.com>
2018-03-08 14:21:41 +0100

    Vistas error 404 y 500

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center">
+        <img class="img-responsive img-fluid img-portfolio img-hover" src="{{ asset('500.gif') }}">
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,62 +11,79 @@
 |
 */
 
-Route::get('/home', 'UsersController@subjectsUser')->name('inicioLogin')->middleware('auth');
 Route::get('/', 'PagesController@home')->name('inicioNoLogin')->middleware('guest');
 
 //Rutas asincronas
 Route::get('/giveEvents/', 'EventsController@givePageEvents');
 Route::get('/giveMyEvents/', 'UsersController@givePageMyEvents');
-Route::post('/votar', 'VotesController@votar')->middleware('auth');
-Route::post('/comentar', 'CommentarysController@comentar')->middleware('auth');
 Route::post('/allcomments', 'CommentarysController@allcoments');
-Route::post('/reservar', 'ReservesController@validacionAjax')->middleware('auth');
-Route::post('/factura', 'InvoicesController@factura')->middleware('auth');
-Route::get('/profile/events', 'UsersController@eventsProfile')->middleware('auth');
-Route::get('/profile/info', 'UsersController@profileInfo')->middleware('auth');
 
 //Rutas de eventos
 Route::get('/events/', 'EventsController@index');
-Route::get('/events/create', 'EventsController@create')->middleware('auth');
 Route::get('/events/{event}', 'EventsController@show');
-Route::post('/events/create', 'EventsController@store')->middleware('auth');
-
-//Rutas de reservas
-Route::get('/events/{event}/reserves', 'ReservesController@index')->middleware('auth');
-Route::get('/events/{event}/reserves/create', 'ReservesController@create')->middleware('auth');
-Route::get('/events/{event}/reserves/{reserve}', 'ReservesController@show')->middleware('auth');
-Route::post('/events/{event}/reserves/create', 'ReservesController@store')->middleware('auth');
 
 //Rutas de votos
-Route::get('/events/{event}/votes/create', 'VotesController@create')->middleware('auth');
 Route::get('/events/{event}/votes', 'VotesController@show');
-Route::post('/events/{event}/votes/create', 'VotesController@createOrEdit')->middleware('auth');
-Route::get('/profile/votes', 'VotesController@index')->middleware('auth');
-
-//Rutas de facturas
-Route::get('/profile/invoices', 'InvoicesController@index')->middleware('auth');
-Route::get('/profile/invoices/{invoice}', 'InvoicesController@show')->middleware('auth');
 
 //Rutas de lugares
 Route::get('/places', 'PlacesController@index');
-Route::get('/places/create', 'PlacesController@create')->middleware('auth');
 Route::get('/places/{place}', 'PlacesController@show');
-Route::post('/places/create', 'PlacesController@store')->middleware('auth');
 
 //Rutas de comentarios
-Route::get('/events/{event}/commentarys/create', 'CommentarysController@create')->middleware('auth');
 Route::get('/events/{event}/commentarys/{commentary}', 'CommentarysController@show');
-Route::post('/events/{user}/commentarys/create', 'CommentarysController@store')->middleware('auth');
-Route::get('/profile/commentarys', 'CommentarysController@index')->middleware('auth');
-Route::get('/profile/commentarys/{commentary}', 'EventsController@usercommentary')->middleware('auth');
-
-//Rutas de temas
-Route::get('/subjects/{subject}', 'SubjectsController@index')->middleware('auth');
-Route::get('/events/{event}/subjects/create', 'SubjectsController@store')->middleware('auth');
-Route::post('/profile/subjects/create', 'SubjectsController@create')->middleware('auth');
 
 //Rutas de usuario
-Route::get('/profile', 'UsersController@profile')->middleware('auth');
 Route::get('/user/{user}', 'UsersController@index')->name('user.profile');
 
+
 Auth::routes();
+
+Route::group(['middleware' => 'auth'], function () {
+
+    Route::get('/home', 'UsersController@subjectsUser')->name('inicioLogin');
+
+    //Rutas asincronas
+    Route::post('/votar', 'VotesController@votar');
+    Route::post('/comentar', 'CommentarysController@comentar');
+    Route::post('/reservar', 'ReservesController@validacionAjax');
+    Route::post('/factura', 'InvoicesController@factura');
+    Route::get('/profile/events', 'UsersController@eventsProfile');
+    Route::get('/profile/info', 'UsersController@profileInfo');
+
+    //Rutas de eventos
+    Route::get('/events/create', 'EventsController@create');
+    Route::post('/events/create', 'EventsController@store');
+
+    //Rutas de reservas
+    Route::get('/events/{event}/reserves', 'ReservesController@index');
+    Route::get('/events/{event}/reserves/create', 'ReservesController@create');
+    Route::get('/events/{event}/reserves/{reserve}', 'ReservesController@show');
+    Route::post('/events/{event}/reserves/create', 'ReservesController@store');
+
+    //Rutas de votos
+    Route::get('/events/{event}/votes/create', 'VotesController@create');
+    Route::post('/events/{event}/votes/create', 'VotesController@createOrEdit');
+    Route::get('/profile/votes', 'VotesController@index');
+
+    //Rutas de facturas
+    Route::get('/profile/invoices', 'InvoicesController@index');
+    Route::get('/profile/invoices/{invoice}', 'InvoicesController@show');
+
+    //Rutas de lugares
+    Route::get('/places/create', 'PlacesController@create');
+    Route::post('/places/create', 'PlacesController@store');
+
+    //Rutas de comentarios
+    Route::get('/events/{event}/commentarys/create', 'CommentarysController@create');
+    Route::post('/events/{user}/commentarys/create', 'CommentarysController@store');
+    Route::get('/profile/commentarys', 'CommentarysController@index')->middleware('auth');
+    Route::get('/profile/commentarys/{commentary}', 'EventsController@usercommentary');
+
+    //Rutas de temas
+    Route::get('/subjects/{subject}', 'SubjectsController@index');
+    Route::get('/events/{event}/subjects/create', 'SubjectsController@store');
+    Route::post('/profile/subjects/create', 'SubjectsController@create');
+
+    //Rutas de usuario
+    Route::get('/profile', 'UsersController@profile');
+});
